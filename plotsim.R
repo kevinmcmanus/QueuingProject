7598767a58e75d6c50d9213889df54e3cf83f6ed
kevinmcmanus <kevin.leo.mcmanus@gmail.com>
2016-04-05 11:41:44 -0600

    plot tweaks
    adjusted placement of text boxes relative to the y-scale

--- plotsim.R
+++ plotsim.R
@@ -9,9 +9,10 @@ plotsim <- function(sim){
                 "\nService Time stddev: ", sim$inputs$svc.sd, " minutes", sep="")
     ggplot(aes(x=t, y=q), data=sim$q)+
         geom_line(size=1, color = "black")+
-        geom_segment(aes(x=sim$last.cust.arr,xend=sim$last.cust.arr, y=0,yend=max(sim$q$q)+1.8),size=2,color="red")+
+        geom_segment(aes(x=sim$last.cust.arr,xend=sim$last.cust.arr, y=0,yend=max(sim$q$q)*1.3),size=2,color="red")+
+        scale_y_continuous(limits = c(0,max(sim$q$q)*2))+
         labs(x="Time (Hours)",y="Queue Length", title = "Queue Length at Time t")+
-        annotate("text",x=sim$last.cust.arr,y=max(sim$q$q)+2,label="Last Arrival",color="red")+
-        annotate("text", hjust=0, vjust=1, x=0, y=max(sim$q$q)+3,label=instr, color="blue")+
-        annotate("text", hjust=1, vjust=1, x=max(sim$q$t), y=max(sim$q$q)+3,label=outstr, color="red")
+        annotate("text",x=sim$last.cust.arr,y=max(sim$q$q)*1.31, vjust=0,label="Last Arrival",color="red")+
+        annotate("text", hjust=0, vjust=1, x=0, y=max(sim$q$q)*2,label=instr, color="blue")+
+        annotate("text", hjust=1, vjust=1, x=max(sim$q$t), y=max(sim$q$q)*2,label=outstr, color="red")
 }
